02c8ea1feb23bef1136eeeacdfd504b5de400a6a
Leslie Samuel Kingsly <leslie.wme.me@gmail.com>
2018-04-13 15:03:48 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import TaskList from './components/TaskList';
+import TaskCard from './components/TaskCard';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+
+const data = require('./Board.data');
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = data;
+	}
+
+	render() {
+		const lists = this.state.lists.map((list) => {
+			return (
+				<div style={{margin: '5px'}} key={list.id}>
+					<TaskList
+						title={list.title}
+						footer={<FlatButton fullWidth label="Add a card" />}>
+						{list.tasks.map((task) => {
+								return (
+									<TaskCard value={task.title} key={task.id} />
+								);
+						})}
+					</TaskList>
+				</div>
+			);
+		});
+
+		/*const tasks = this.state.lists[0].tasks;
+
+		const cards = tasks.map((task) => {
+			return (
+				<TaskCard value={task.title} key={task.id} />
+			);
+		});
+		*/
+		return (
+			<MuiThemeProvider>
+				<div style={{display: 'flex'}}>
+					{lists}
+					<div style={{margin: '5px'}} key="0">
+						<FlatButton
+							label="Add list"
+							primary
+							style={{backgroundColor: '#333', height: '58px', color: 'white', width: '300px'}}
+						/>
+					</div>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
+}
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {List} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import TextField from 'material-ui/TextField';
+//import FlatButton from 'material-ui/FlatButton';
+
+import VirtualizedList from 'react-virtualized/dist/commonjs/List';
+
+export default class TaskList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: this.props.title || ''
+		}
+	}
+
+	static get propTypes() {
+		return {
+			onTitleChange: PropTypes.func,
+			titlePlaceholder: PropTypes.string,
+			footer: PropTypes.element
+		}
+	}
+
+	handleTitleChange(event, newValue) {
+		const title = newValue;
+		this.setState({title});
+	}
+
+	handleTitleKeyUp(event) {
+		switch(event.keyCode) {
+			case 27:
+				this.resetTitle();
+				return;
+			case 13:
+				this.changeTitle(event.target.value);
+				return;
+			default:
+				return "";
+		}
+	}
+
+	changeTitle(newValue) {
+		if(this.props.onTitleChange) {
+			this.props.onTitleChange(newValue);
+		}
+	}
+
+	resetTitle() {
+		const title = this.props.title;
+		this.setState({title});
+	}
+
+	render() {
+		const listItems = this.props.children ? this.props.children.map((child, index) => {
+			return (
+				<div style={{marginBottom: '5px'}} key={index}> {/*FIXME: Use appropriate key here*/}
+					{child}
+				</div>
+			);
+		}) : null;
+
+		const listRenderer = ({
+			index,
+			style
+		}) => {
+			return (
+				<div style={style}  key={index}> {/*FIXME: Use appropriate key here*/}
+					{this.props.children[index]}
+				</div>
+			);
+		};
+
+		return (
+			<List style={{backgroundColor: '#e4e4e4', padding: '5px 10px', borderRadius: '3px'}}>
+				<Subheader>
+					<TextField
+						name="listTitle"
+						style={{fontSize: 'inherit', fontWeight: 'bold'}}
+						placeholder={this.props.titlePlaceholder || ''}
+						value={this.state.title}
+						onChange={(event, newValue) => this.handleTitleChange(event,newValue)}
+						onKeyUp={(event) => {this.handleTitleKeyUp(event)}}
+					/>
+				</Subheader>
+				{listItems}
+				<VirtualizedList
+					width={300}
+					height={400}
+					rowCount={this.props.children.length}
+					overscanRowCount={5}
+					rowHeight={80}
+					rowRenderer={listRenderer}
+				/>
+				{this.props.footer || null}
+			</List>
+		);
+	}
+}
